bbc6b6ba038f24f9a40bddb29a73425bb40fd221
xuanyan <xuanyan1983@gmail.com>
2010-07-19 11:08:38 +0800

    .

--- javascript/api.js
+++ javascript/api.js
@@ -1,3 +1,12 @@
+/*
+ * This file is part of the Geek-Zoo Projects.
+ *
+ * @copyright (c) 2010 Geek-Zoo Projects More info http://www.geek-zoo.com
+ * @license http://opensource.org/licenses/gpl-2.0.php The GNU General Public License
+ * @author xuanyan <xuanyan@geek-zoo.com>
+ *
+ */
+
 // example:
 // curt.cc('http://www.kukufun.com', function(url) {
 //     alert(url);
